a5207770b43b1df88b60011b6926ee0c22ae8988
mengyuan <15332392019@163.com>
2021-08-05 15:37:18 +0800

    [mengyuan] feat: try jQuery

---
+++ jQuery/jQuery.js
@@ -0,0 +1,69 @@
+
+//用户点击button后，所有<p>都隐藏
+$(document).ready(function() {
+    $("#button1").click(function() {
+        $("p").hide();
+    });
+})
+
+//用户点击按钮后，有 id="test" 属性的元素将被隐藏
+$(document).ready(function(){
+    $("#button2").click(function(){
+      $("#test").hide();
+    });
+  });
+
+//用户点击p标签后元素消失
+$(document).ready(function(){
+    $("p").click(function(){
+      $(this).hide();
+    });
+  });  
+
+//鼠标移动到此处会看到弹窗
+$(document).ready(function(){
+    $("#p1").mouseenter(function(){
+      alert('您的鼠标移到了 id="p1" 的元素上!');
+    });
+  });  
+
+//鼠标移动到此处以及离开此处时都将触发function
+$(document).ready(function(){
+    $("#p2").hover(
+		function(){
+			alert("你进入了 p1!");
+		},
+		function(){
+			alert("拜拜! 现在你离开了 p1!");
+		}
+    )
+}); 
+
+//触发focus事件
+$(document).ready(function(){
+    $("input").focus(function(){
+      $(this).css("background-color","#cccccc");
+    });
+    $("input").blur(function(){
+      $(this).css("background-color","#ffffff");
+    });
+  });
+
+
+//hide & show
+$(document).ready(function(){
+    $("#hide").click(function(){
+      $("p").hide();
+    });
+    $("#show").click(function(){
+      $("p").show();
+    });
+  });  
+
+
+//获取Input的值
+$(document).ready(function(){
+    $("#show-input-button").click(function(){
+      alert("值为: " + $("#show-input").val());
+    });
+  });  
