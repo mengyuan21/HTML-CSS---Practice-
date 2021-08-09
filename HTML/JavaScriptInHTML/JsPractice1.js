c953db4a44a8a947cbe54207c54a29873f597b23
mengyuan <15332392019@163.com>
2021-08-09 11:04:13 +0800

    [mengyuan] refactor

--- HTML/JavaScriptInHTML/JsPractice1.js
+++ HTML/JavaScriptInHTML/JsPractice1.js
@@ -1,9 +1,7 @@
  const countryName = ["America", "Greece", "Britain", "Canada", "China"]
-// 重构 
  let number = 0 
 
 document.write("在以下字符中：<br/>");
-//map
 
 countryName.forEach( country => {
     document.write(country)
@@ -12,7 +10,10 @@ document.write('<br/>包含字母A，a的国家有：<br/>')
 
 countryName.forEach( country => {
     if(country.indexOf("A") != -1 || country.indexOf("a") != -1){
+        number++;
         document.write(country)
     }
 })
+document.write('<br/>共'+number+"个")
+
 
